5fdf866a84a5a0bc0cc0a04e7d7e09dbd45f937f
EvisN <paul.lichtenberger@gmx.de>
2014-05-27 12:21:52 +0200

    muell gelpoescht

--- BischofshofPresenter/app/src/main/java/lp/german/bischofshofpresenter/app/MainActivity.java
+++ BischofshofPresenter/app/src/main/java/lp/german/bischofshofpresenter/app/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
-    private ImageButton btnNeuesProjekt, btnProjektBearbeiten, btnEinstellungen, btnPraesentationStarten;
     private FileHandler fileHandler;
 
     @Override
@@ -25,45 +24,13 @@ public class MainActivity extends Activity {
     private void setupUIElements(){
 
         //Navigationsbuttons
-        btnNeuesProjekt = (ImageButton)findViewById(R.id.btn_neues_projekt);
-        btnProjektBearbeiten = (ImageButton)findViewById(R.id.btn_projekt_bearbeiten);
-        btnEinstellungen = (ImageButton)findViewById(R.id.btn_einstellungen);
-        btnPraesentationStarten = (ImageButton)findViewById(R.id.btn_praesentation_starten);
+
     }
 
     private void addUIClickListeners(){
 
         //Intents
-        btnNeuesProjekt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), NeuesProjektActivity.class);
-                startActivity(i);
-            }
-        });
 
-        btnProjektBearbeiten.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), ProjektBearbeitenActivity.class);
-                startActivity(i);
-            }
-        });
-
-        btnEinstellungen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), EinstellungenActivity.class);
-                startActivity(i);
-            }
-        });
-
-        btnPraesentationStarten.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fileHandler.getPräsentationen();
-            }
-        });
 
     }
 }
