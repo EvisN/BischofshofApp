a162c6f95753acb1c0d799550f8f183c2c28d0cf
EvisN <paul.lichtenberger@gmx.de>
2014-07-08 12:51:55 +0200

    preferences menu schliest sich bei wahl der option

--- BischofshofPresenter/app/src/main/java/lp/german/bischofshofpresenter/app/Preferences.java
+++ BischofshofPresenter/app/src/main/java/lp/german/bischofshofpresenter/app/Preferences.java
@@ -1,6 +1,7 @@
 package lp.german.bischofshofpresenter.app;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 
@@ -14,5 +15,14 @@ public class Preferences extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        Preference pref_marke = findPreference("pref_marke");
+        pref_marke.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                getActivity().finish();
+                return true;
+            }
+        });
     }
 }
